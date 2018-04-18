fd926c3f6f2e230a620d0993828e713f0e785a52
Bobby Martin and Garnett Puett <bobby@>
2018-04-18 09:42:21 -0700

    Add first spec test

---
+++ spec/rpg-spec.js
@@ -0,0 +1,9 @@
+import { User } from './../src/rpg.js';
+
+describe('Player', function() {
+
+  it('should test giving the user a level', function() {
+    const player = new User();
+    console.log(player);
+  });
+});
--- src/RPG.js
+++ src/RPG.js
@@ -1,27 +1,39 @@
-export class Player {
-  constructor(health, damage, level, exp) {
+export class User {
+  constructor() {
     this.health = 100;
     this.damage = 0;
     this.level = 1;
     this.exp = 0;
   }
 
-  method() {
+  enemyAttack() {
 
   }
-};
 
+  expGain() {
 
-
-export class Enemy {
-  constructor(health, damage, expGiven) {
-    this.health = health;
-    this.damage = damage;
-    this.expGiven = expGiven;
   }
 
-  method() {
-    
+  levelUp() {
+
   }
 
-};
+}
+
+
+
+// export class Enemy {
+//   constructor(health, damage, expGiven) {
+//     this.health = health;
+//     this.damage = damage;
+//     this.expGiven = 20;
+//   }
+//
+//   userAttack() {
+//
+//   }
+//
+//
+// };
+
+exports.userObject = User;
